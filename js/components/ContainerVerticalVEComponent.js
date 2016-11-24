f2c0f5fe468affdeed077790277e490959a2137c
myj <1368468125@qq.com>
2016-11-24 23:46:42 +0800

    修正普通容器组件

--- js/components/ContainerVerticalVEComponent.js
+++ js/components/ContainerVerticalVEComponent.js
@@ -3,6 +3,9 @@
  */
 function ContainerVerticalVEComponent(config){
     config = config ? config : {};
+    $.extend(true, config, {
+        containerClassName: componentContainerClassName + " " + layoutContainerClassName
+    });
     var args = [].slice.call(arguments, 0);
     ContainerVEComponent.apply(this, args);
 
@@ -48,42 +51,42 @@ function ContainerVerticalVEComponent(config){
         propName: "列数",
         propVal: 1
     });
-    var veColumnConfig = {
-        containerClassName: componentContainerClassName + " " + layoutContainerClassName + " " + childComponentClassName,
-    };
-    var colNum = this.getControlItem({
-        propLevel1: "otherAttrs",
-        propLevel2: "colNum"
-    }).propVal;
-    var oVEComponent;
-    this.childComponents = [];
-    for(var i=0;i<colNum;i++){
-        oVEComponent = new VEComponent(veColumnConfig);
-        oVEComponent.setControlItem({
-            propLevel1: "css",
-            propLevel2: "width",
-            propVal: 1 / colNum * 100 + "%"
-        },{
-            propLevel1: "css",
-            propLevel2: "height",
-            propVal: "400px"
-        },{
-            propLevel1: "css",
-            propLevel2: "position",
-            propVal: "relative"
-        },{
-            propLevel1: "css",
-            propLevel2: "display",
-            propVal: "inline-block"
-        },{
-            propLevel1: "css",
-            propLevel2: "backgroundColor",
-            propVal: "#eeeeff"
-        },{
-            propLevel1: "css",
-            propLevel2: "boxSizing",
-            propVal: "border-box"
-        });
-        this.childComponents.push(oVEComponent);
-    }
+    // var veColumnConfig = {
+    //     containerClassName: componentContainerClassName + " " + layoutContainerClassName + " " + childComponentClassName,
+    // };
+    // var colNum = this.getControlItem({
+    //     propLevel1: "otherAttrs",
+    //     propLevel2: "colNum"
+    // }).propVal;
+    // var oVEComponent;
+    // this.childComponents = [];
+    // for(var i=0;i<colNum;i++){
+    //     oVEComponent = new VEComponent(veColumnConfig);
+    //     oVEComponent.setControlItem({
+    //         propLevel1: "css",
+    //         propLevel2: "width",
+    //         propVal: 1 / colNum * 100 + "%"
+    //     },{
+    //         propLevel1: "css",
+    //         propLevel2: "height",
+    //         propVal: "400px"
+    //     },{
+    //         propLevel1: "css",
+    //         propLevel2: "position",
+    //         propVal: "relative"
+    //     },{
+    //         propLevel1: "css",
+    //         propLevel2: "display",
+    //         propVal: "inline-block"
+    //     },{
+    //         propLevel1: "css",
+    //         propLevel2: "backgroundColor",
+    //         propVal: "#eeeeff"
+    //     },{
+    //         propLevel1: "css",
+    //         propLevel2: "boxSizing",
+    //         propVal: "border-box"
+    //     });
+    //     this.childComponents.push(oVEComponent);
+    // }
 }
